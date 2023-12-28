68da7df659baa11c872a3b130c2fc0bd5c12a5d5
Maschell <Maschell@gmx.de>
2023-12-28 13:45:31 +0100

    WUPS 0.8.0 support

--- src/retain_vars.cpp
+++ src/retain_vars.cpp
@@ -1,12 +1,13 @@
 #include "retain_vars.hpp"
-#include <vpad/input.h>
+#include "utils/config.h"
 
-uint32_t gSwapScreenButtonCombo         = VPAD_BUTTON_TV;
-uint32_t gSwapAudioButtonCombo          = VPAD_BUTTON_STICK_L;
-bool gSwapScreenButtonComboEnabled      = true;
-bool gChangeAudioModeButtonComboEnabled = false;
-bool gEnabled                           = true;
-bool gDoScreenSwap                      = false;
-bool gShowNotifications                 = true;
-bool gNotificationModuleInitDone        = true;
-SwipSwapAudioMode gCurAudioMode         = AUDIO_MODE_MATCH_SCREEN;
+bool gEnabled                           = DEFAULT_ENABLED_CONFIG_VALUE;
+bool gDoScreenSwap                      = DEFAULT_SWAP_SCREENS_CONFIG_VALUE;
+uint32_t gSwapScreenButtonCombo         = DEFAULT_SWAP_SCREEN_BUTTON_COMBO_CONFIG_VALUE;
+uint32_t gSwapAudioButtonCombo          = DEFAULT_CHANGE_AUDIO_BUTTON_COMBO_CONFIG_VALUE;
+bool gSwapScreenButtonComboEnabled      = DEFAULT_ENABLED_SWAP_SCREENS_COMBO_CONFIG_VALUE;
+bool gChangeAudioModeButtonComboEnabled = DEFAULT_ENABLED_CHANGE_AUDIO_COMBO_CONFIG_VALUE;
+bool gShowNotifications                 = DEFAULT_ENABLE_NOTIFICATIONS_CONFIG_VALUE;
+SwipSwapAudioMode gCurAudioMode         = DEFAULT_AUDIO_MODE_CONFIG_VALUE;
+
+bool gNotificationModuleInitDone = true;
--- src/utils/config.h
+++ src/utils/config.h
@@ -1,53 +1,29 @@
 #pragma once
 #include "logger.h"
+#include "retain_vars.hpp"
 #include <string>
+#include <vpad/input.h>
 #include <wups/config/WUPSConfigItemBoolean.h>
 #include <wups/storage.h>
 
-#define ENABLED_CONFIG_STRING                    "enabled"
-#define SWAP_SCREENS_CONFIG_STRING               "swapScreens"
-#define ENABLED_SWAP_SCREENS_COMBO_CONFIG_STRING "swapScreensComboEnabled"
-#define ENABLED_CHANGE_AUDIO_COMBO_CONFIG_STRING "changeAudioComboEnabled"
-#define ENABLE_NOTIFICATIONS_CONFIG_STRING       "notificationsEnabled"
-#define SWAP_SCREEN_BUTTON_COMBO_CONFIG_STRING   "screenButtonCombo"
-#define CHANGE_AUDIO_BUTTON_COMBO_CONFIG_STRING  "audioButtonCombo"
-#define SCREEN_MODE_CONFIG_STRING                "audioMode"
+#define DEFAULT_ENABLED_CONFIG_VALUE                    true
+#define DEFAULT_SWAP_SCREENS_CONFIG_VALUE               false
+#define DEFAULT_ENABLED_SWAP_SCREENS_COMBO_CONFIG_VALUE true
+#define DEFAULT_ENABLED_CHANGE_AUDIO_COMBO_CONFIG_VALUE false
+#define DEFAULT_ENABLE_NOTIFICATIONS_CONFIG_VALUE       true
+#define DEFAULT_SWAP_SCREEN_BUTTON_COMBO_CONFIG_VALUE   VPAD_BUTTON_TV
+#define DEFAULT_CHANGE_AUDIO_BUTTON_COMBO_CONFIG_VALUE  VPAD_BUTTON_STICK_L
+#define DEFAULT_AUDIO_MODE_CONFIG_VALUE                 AUDIO_MODE_MATCH_SCREEN
 
-#define LOAD_BOOL_FROM_STORAGE(config_name, __variable__)                                                         \
-    if ((storageRes = WUPS_GetBool(nullptr, config_name, &__variable__)) == WUPS_STORAGE_ERROR_NOT_FOUND) {       \
-        if (WUPS_StoreBool(nullptr, config_name, __variable__) != WUPS_STORAGE_ERROR_SUCCESS) {                   \
-            DEBUG_FUNCTION_LINE_WARN("Failed to store bool");                                                     \
-        }                                                                                                         \
-    } else if (storageRes != WUPS_STORAGE_ERROR_SUCCESS) {                                                        \
-        DEBUG_FUNCTION_LINE_WARN("Failed to get bool %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes); \
-    }                                                                                                             \
-    while (0)
+#define ENABLED_CONFIG_STRING                           "enabled"
+#define SWAP_SCREENS_CONFIG_STRING                      "swapScreens"
+#define ENABLED_SWAP_SCREENS_COMBO_CONFIG_STRING        "swapScreensComboEnabled"
+#define ENABLED_CHANGE_AUDIO_COMBO_CONFIG_STRING        "changeAudioComboEnabled"
+#define ENABLE_NOTIFICATIONS_CONFIG_STRING              "notificationsEnabled"
+#define SWAP_SCREEN_BUTTON_COMBO_CONFIG_STRING          "screenButtonCombo"
+#define CHANGE_AUDIO_BUTTON_COMBO_CONFIG_STRING         "audioButtonCombo"
+#define AUDIO_MODE_CONFIG_STRING                        "audioMode"
 
-#define LOAD_STRING_FROM_STORAGE(config_name, __string, __string_length)                                                  \
-    if ((storageRes = WUPS_GetString(nullptr, config_name, __string, __string_length)) == WUPS_STORAGE_ERROR_NOT_FOUND) { \
-        if (WUPS_StoreString(nullptr, config_name, __string) != WUPS_STORAGE_ERROR_SUCCESS) {                             \
-            DEBUG_FUNCTION_LINE_WARN("Failed to store string");                                                           \
-        }                                                                                                                 \
-    } else if (storageRes != WUPS_STORAGE_ERROR_SUCCESS) {                                                                \
-        DEBUG_FUNCTION_LINE_WARN("Failed to get bool %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes);         \
-    }                                                                                                                     \
-    while (0)
+WUPSConfigAPICallbackStatus ConfigMenuOpenedCallback(WUPSConfigCategoryHandle rootHandle);
 
-#define LOAD_INT_FROM_STORAGE(config_name, __intValue)                                                               \
-    if ((storageRes = WUPS_GetInt(nullptr, config_name, (int32_t *) &__intValue)) == WUPS_STORAGE_ERROR_NOT_FOUND) { \
-        if (WUPS_StoreInt(nullptr, config_name, (int32_t) __intValue) != WUPS_STORAGE_ERROR_SUCCESS) {               \
-            DEBUG_FUNCTION_LINE_WARN("Failed to store int");                                                         \
-        }                                                                                                            \
-    } else if (storageRes != WUPS_STORAGE_ERROR_SUCCESS) {                                                           \
-        DEBUG_FUNCTION_LINE_WARN("Failed to get int %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes);     \
-    }                                                                                                                \
-    while (0)
-
-#define PROCESS_BOOL_ITEM_CHANGED(__config__name, __variable__)                   \
-    if (std::string_view(item->configId) == __config__name) {                     \
-        DEBUG_FUNCTION_LINE_ERR("New value in %s: %d", __config__name, newValue); \
-        __variable__ = newValue;                                                  \
-        WUPS_StoreInt(nullptr, __config__name, __variable__);                     \
-        return;                                                                   \
-    }                                                                             \
-    while (0)
+void ConfigMenuClosedCallback();
