0221a05d96741267e9503e5c71ac56e26ef846f9
Maschell <Maschell@gmx.de>
2024-03-03 18:04:14 +0100

    Add support for mirroring the TV/DRC

--- src/utils/config.h
+++ src/utils/config.h
@@ -7,16 +7,18 @@
 #include <wups/storage.h>
 
 #define DEFAULT_ENABLED_CONFIG_VALUE                    true
-#define DEFAULT_SWAP_SCREENS_CONFIG_VALUE               false
 #define DEFAULT_ENABLED_SWAP_SCREENS_COMBO_CONFIG_VALUE true
 #define DEFAULT_ENABLED_CHANGE_AUDIO_COMBO_CONFIG_VALUE false
 #define DEFAULT_ENABLE_NOTIFICATIONS_CONFIG_VALUE       true
 #define DEFAULT_SWAP_SCREEN_BUTTON_COMBO_CONFIG_VALUE   VPAD_BUTTON_TV
 #define DEFAULT_CHANGE_AUDIO_BUTTON_COMBO_CONFIG_VALUE  VPAD_BUTTON_STICK_L
+
+#define DEFAULT_SCREEN_MODE_CONFIG_VALUE                SCREEN_MODE_NONE
 #define DEFAULT_AUDIO_MODE_CONFIG_VALUE                 AUDIO_MODE_MATCH_SCREEN
 
 #define ENABLED_CONFIG_STRING                           "enabled"
-#define SWAP_SCREENS_CONFIG_STRING                      "swapScreens"
+#define SWAP_SCREENS_CONFIG_STRING_DEPRECATED           "swapScreens"
+#define SCREEN_MODE_CONFIG_STRING                       "screenMode"
 #define ENABLED_SWAP_SCREENS_COMBO_CONFIG_STRING        "swapScreensComboEnabled"
 #define ENABLED_CHANGE_AUDIO_COMBO_CONFIG_STRING        "changeAudioComboEnabled"
 #define ENABLE_NOTIFICATIONS_CONFIG_STRING              "notificationsEnabled"
